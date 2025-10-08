44025d502d92fa7b546b39d7a369c583891f3b87
SanthiyaMeganathan <meganathankmsanthiya@gmail.com>
2025-10-08 21:08:38 +0530

    delete at start and end

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -14,14 +14,12 @@ class Node{
 }
 
 class LinkedList{
-
     Node head;
     Node tail;
 
     //function to insert at begniing:
 
     public void insertAtBeginning(int value){
-
         Node newNode = new Node(value);
         newNode.next =head;
         head=newNode;
@@ -30,19 +28,14 @@ class LinkedList{
 
     //function to insert the node at end:
 
-    public void insertAtEnd(int value){
-
-      
-
+    public void insertAtEnd(int value){   
         Node newNode = new Node(value);
-
         if(head==null){
             newNode.next=head;
             head=newNode;
             tail=newNode;
             return;
         }
-
         // using tail traversing and inserting at end
 
         /* Node temp =head;
@@ -64,11 +57,8 @@ class LinkedList{
         temp.next=newNode;*/
 
         //with tail without traversing:
-
         tail.next = newNode;
         tail=newNode;
-
-
     }
 
     //insert at any postion:
@@ -76,21 +66,14 @@ class LinkedList{
         Node newNode = new Node( value);
         System.out.println("Enter the position you want to insert the element in the list");
         Scanner input = new Scanner(System.in);
-        int position =input.nextInt();
-
-        
+        int position =input.nextInt();      
         int count =1;
         Node temp1=head;
         Node temp =head;
-        
-
         while(temp1.next!=null){
             count = count+1;
             temp1=temp1.next;
         }
-
-        
-
         if(position>=count){
             temp1.next=newNode;
             tail=newNode;
@@ -98,40 +81,70 @@ class LinkedList{
         }else if(position<=0){
             newNode.next=head;
             head=newNode;
-        }
-        
+        }       
         else{
         int index=0;
         Node first=head;
         Node last=head.next;
-        
-
         while(index<position){
 
             first=temp;
             last=temp.next;
             temp = temp.next;
             index=index+1;
-
-
         }
-
         first.next =newNode;
         newNode.next=last;
-
         }
-    
+   
+    }
 
+    //delete from begining:
+
+    public void deleteBeginning(){
+        
+        if(head ==null){
+            System.out.println(" ");
+            System.out.println("There is nothing to delete");
+            return;
+        }
+
+        head =head.next;
+    }
+
+    //delete at end:
+
+    public void deleteAtEnd(){
+
+        Node temp=head;
+        Node lastBefore =head;
         
 
-        
+        if(head==null){
+            System.out.println("There is nothing to delete");
+            return;
+        }else if(head.next==null){
+            System.out.println("You had only one element that too deleted");
+            head=null;
+            return;
+            
 
+        }else{
+            while(temp.next!=null){
+                lastBefore=temp;
+                temp =temp.next;
+            }
+
+          
+            lastBefore.next=null;
+
+            System.out.println("one element dleted from last");
+        }
         
     }
 
-   
-
-
+    
+    //to print the elements in linked list;
 
     public void printlist(){
         Node temp =head;
--- LinkedList/Main.java
+++ LinkedList/Main.java
@@ -1,7 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        //list we used to insert at front:
         LinkedList list = new LinkedList();
+        // list we used to insert at end and anywhere
         LinkedList list1 =new LinkedList();
+        //list we use to check what will happen will the list is empty and we try to remove anything:
+        //null list;
+        LinkedList list2 = new LinkedList();
+        //list we use to check what if we remove the first element
+        LinkedList list3 = new LinkedList();
+        //list to check to deletefrom end when list as one element:
+      
 
         list.insertAtBeginning(10);
         list.insertAtBeginning(12);
@@ -25,6 +34,56 @@ public class Main {
 
         
         list1.printlist();
+
+        list2.deleteBeginning();
+
+        //inserting to list3 to check:
+
+        list3.insertAtBeginning(1);
+        list3.insertAtBeginning(2);
+        list3.insertAtBeginning(3);
+        list3.insertAtBeginning(4);
+
+        System.out.println("The list 3 before deleting at beginning");
+
+        list3.printlist();
+
+        list3.deleteBeginning();
+
+        System.out.println("The list 3 before deleting at beginning");
+
+        list3.printlist();
+
+        LinkedList list4 = new LinkedList();
+        LinkedList list5 = new LinkedList();
+
+        //list to check to delete from end:
+        LinkedList list6 = new LinkedList();
+
+        list4.deleteAtEnd();
+
+        list5.insertAtBeginning(1);
+
+       list5.printlist();
+
+       list5.deleteAtEnd();
+
+       list6.insertAtEnd(2);
+        list6.insertAtEnd(3);
+         list6.insertAtEnd(4);
+          list6.insertAtEnd(6);
+           list6.insertAtEnd(7);
+
+           System.out.println("bf deletion");
+
+           list6.printlist();
+           list6.deleteAtEnd();
+
+           System.out.println("after deletion");
+           list6.printlist();
+
+        
+
 
 
 
