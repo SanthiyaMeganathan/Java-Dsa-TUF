5dfa36089ffc20a457fb461c5d5a92afe0a587b8
SanthiyaMeganathan <meganathankmsanthiya@gmail.com>
2025-10-08 19:46:28 +0530

    Linkedlist : insertAtFirst, End , anyPosition

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -1,3 +1,6 @@
+import java.util.*;
+
+
 class Node{
 
     int data;
@@ -13,6 +16,7 @@ class Node{
 class LinkedList{
 
     Node head;
+    Node tail;
 
     //function to insert at begniing:
 
@@ -23,6 +27,111 @@ class LinkedList{
         head=newNode;
        
     }
+
+    //function to insert the node at end:
+
+    public void insertAtEnd(int value){
+
+      
+
+        Node newNode = new Node(value);
+
+        if(head==null){
+            newNode.next=head;
+            head=newNode;
+            tail=newNode;
+            return;
+        }
+
+        // using tail traversing and inserting at end
+
+        /* Node temp =head;
+
+        // while(temp!=null){
+        //     tail = temp;
+        //     temp =temp.next;
+
+        // }
+        tail.next=newNode;*/
+
+        // without tail:
+
+        /*Node temp = head;
+
+        while(temp.next!=null){
+            temp = temp.next;
+        }
+        temp.next=newNode;*/
+
+        //with tail without traversing:
+
+        tail.next = newNode;
+        tail=newNode;
+
+
+    }
+
+    //insert at any postion:
+    public void insertAtAnyPosition(int value){
+        Node newNode = new Node( value);
+        System.out.println("Enter the position you want to insert the element in the list");
+        Scanner input = new Scanner(System.in);
+        int position =input.nextInt();
+
+        
+        int count =1;
+        Node temp1=head;
+        Node temp =head;
+        
+
+        while(temp1.next!=null){
+            count = count+1;
+            temp1=temp1.next;
+        }
+
+        
+
+        if(position>=count){
+            temp1.next=newNode;
+            tail=newNode;
+            
+        }else if(position<=0){
+            newNode.next=head;
+            head=newNode;
+        }
+        
+        else{
+        int index=0;
+        Node first=head;
+        Node last=head.next;
+        
+
+        while(index<position){
+
+            first=temp;
+            last=temp.next;
+            temp = temp.next;
+            index=index+1;
+
+
+        }
+
+        first.next =newNode;
+        newNode.next=last;
+
+        }
+    
+
+        
+
+        
+
+        
+    }
+
+   
+
+
 
     public void printlist(){
         Node temp =head;
--- LinkedList/Main.java
+++ LinkedList/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
+        LinkedList list1 =new LinkedList();
 
         list.insertAtBeginning(10);
         list.insertAtBeginning(12);
@@ -8,6 +9,26 @@ public class Main {
 
         System.out.println("The elements in the Linked List");
         list.printlist();
+
+        System.out.println("Elements which is begining insterin end");
+
+        list1.insertAtEnd(2);
+        list1.insertAtEnd(4);
+        list1.insertAtEnd(5);
+
+        System.out.println("Elements in the List1;  ");
+
+        list1.printlist();
+
+        System.out.println("Enter the element at any position");
+        list1.insertAtAnyPosition(8);
+
+        
+        list1.printlist();
+
+
+
+    
 
        
     }
